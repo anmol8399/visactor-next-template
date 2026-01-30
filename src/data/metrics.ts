2db78bd8ee1a2a7b97bf51ae2b108b55eaafd90b
Anmol Gulati <anmolgulati8399@gmail.com>
2026-01-31 01:45:30 +0530

    Update metrics.ts

--- src/data/metrics.ts
+++ src/data/metrics.ts
@@ -1,22 +1,22 @@
 export const metrics = [
   {
-    title: "Created Tickets",
-    value: "24,208",
+    title: "Total Applicants",
+    value: "1,208",
     change: -0.05,
   },
   {
-    title: "Unsolved Tickets",
-    value: "4,564",
+    title: "Unique Applicants",
+    value: "964",
     change: 0.02,
   },
   {
-    title: "Resolved Tickets",
-    value: "18,208",
+    title: "Shortlisted Candidates",
+    value: "208",
     change: 0.08,
   },
   {
-    title: "Average First Time Reply",
-    value: "12:01 min",
-    change: 0.08,
+    title: "Average Time to Hire",
+    value: "12 days",
+    change: -0.08,
   },
 ];
